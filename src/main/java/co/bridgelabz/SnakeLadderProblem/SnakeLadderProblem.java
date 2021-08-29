53abf4b080abfdaf7eaad9cfafd04295334cb92c
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-29 21:32:42 +0530

    Checks for a option

--- src/main/java/co/bridgelabz/SnakeLadderProblem/SnakeLadderProblem.java
+++ src/main/java/co/bridgelabz/SnakeLadderProblem/SnakeLadderProblem.java
@@ -3,12 +3,37 @@ package co.bridgelabz.SnakeLadderProblem;
 
 public class SnakeLadderProblem 
 {
-	public static void main( String[] args )
+	static int INITIAL_POSITION = 0;
+	int numberOfPlayer = 1;
+	
+	void checkOption(int playerPosition)
+	{
+		int diceNumber, option;
+        diceNumber = (int)(Math.random()*6+1);
+        System.out .println("Dice throw: " +diceNumber);
+        option = (int)(Math.random()*3);
+        switch(option)
+        {
+        case 0:
+        	playerPosition = playerPosition;
+        	System.out.println("NO PLAY\n Player's current position: " +playerPosition);
+        	break;
+        case 1:
+        	playerPosition = playerPosition + diceNumber;
+        	System.out.println("Ladder\n Player's current position: " +playerPosition);
+        	break;
+        case 2:
+        	playerPosition = playerPosition - diceNumber;
+        	System.out.println("Snake\n Player's current position: " +playerPosition);
+        	break;
+        }
+	}
+    public static void main( String[] args )
     {
         System.out.println( "Welcome to SNAKE AND LADDER GAME" );
-        int INITIAL_POSITION = 0, numberOfPlayer = 1, diceNumber;
         System.out.println("Game starts at Player Position: " +INITIAL_POSITION);
-        diceNumber = (int)(Math.random()*6+1);
-        System.out .println(diceNumber);
+        int playerPosition = INITIAL_POSITION;
+        SnakeLadderProblem ob = new SnakeLadderProblem();
+        ob.checkOption(playerPosition);
     }
 }
