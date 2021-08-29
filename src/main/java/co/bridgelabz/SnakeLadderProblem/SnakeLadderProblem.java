0218f767d721d7a374ff0194d9feaf83a10cfdf1
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-29 21:28:24 +0530

    Start position 0

--- src/main/java/co/bridgelabz/SnakeLadderProblem/SnakeLadderProblem.java
+++ src/main/java/co/bridgelabz/SnakeLadderProblem/SnakeLadderProblem.java
@@ -3,8 +3,10 @@ package co.bridgelabz.SnakeLadderProblem;
 
 public class SnakeLadderProblem 
 {
-    public static void main( String[] args )
+	public static void main( String[] args )
     {
-        System.out.println( "Welcome to Snake and Ladder Game" );
+        System.out.println( "Welcome to SNAKE AND LADDER GAME" );
+        int INITIAL_POSITION = 0, numberOfPlayer = 1;
+        System.out.println("Game starts at Player Position: " +INITIAL_POSITION);
     }
 }
