1392103d4ef240c6d76649eec35e8faf9e7055eb
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-29 21:24:51 +0530

    Welcome Message

---
+++ src/main/java/co/bridgelabz/SnakeLadderProblem/SnakeLadderProblem.java
@@ -0,0 +1,10 @@
+package co.bridgelabz.SnakeLadderProblem;
+
+
+public class SnakeLadderProblem 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Snake and Ladder Game" );
+    }
+}
