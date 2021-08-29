579a1f14ac0babde408f2ec9ab8c3215c71983aa
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-29 21:30:31 +0530

    Rolls the die

--- src/main/java/co/bridgelabz/SnakeLadderProblem/SnakeLadderProblem.java
+++ src/main/java/co/bridgelabz/SnakeLadderProblem/SnakeLadderProblem.java
@@ -6,7 +6,9 @@ public class SnakeLadderProblem
 	public static void main( String[] args )
     {
         System.out.println( "Welcome to SNAKE AND LADDER GAME" );
-        int INITIAL_POSITION = 0, numberOfPlayer = 1;
+        int INITIAL_POSITION = 0, numberOfPlayer = 1, diceNumber;
         System.out.println("Game starts at Player Position: " +INITIAL_POSITION);
+        diceNumber = (int)(Math.random()*6+1);
+        System.out .println(diceNumber);
     }
 }
